dfc45a47f0c10b3b3d6c0db089533b42271f2153
SKU20 <121404065+SKU20@users.noreply.github.com>
2024-09-05 12:41:54 +0400

    final

--- src/database.js
+++ src/database.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('laundry_db', 'root', 'baza', {
-  host: '127.0.0.1',
-  dialect: 'mysql'
-});
-
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  }
+);
 
 sequelize.authenticate()
   .then(() => {
